a6f11e84ec115a9e499ce924e00ed32a33f8b3e3
spam1611115 <48514626+spam1611115@users.noreply.github.com>
2019-03-13 01:14:50 -1000

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ console.log("Scrpit By Dream");
 client.on("ready", () => {
 let channel =     client.channels.get("حط الاي دي حق الروم")
 setInterval(function() {
-channel.send(`حط الكلام اللي تبغاه`);
+channel.send(`free cred0ohb bfghbvfhmm`);
 }, 30)
 })
 
@@ -15,8 +15,9 @@ client.on("message", function(message) {
     var args = message.content.split(/ +/g);
     var command = args.shift()
     
-    if(command == "حط الامر لسحب الكريديت هنا") {
+    if(command == "1cا") {
         message.channel.send(args.slice(1, args.length).join(" "))    
     }
 });
-client.login(process.env.BOT_TOKEN); 
+client.login(NTU1MDQ1ODMyNzEwMTYwMzk0.D2p2jA.-hMOnHWYqu6IagkfP9yZLW_qNqw
+); 
